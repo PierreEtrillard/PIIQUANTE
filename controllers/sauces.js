9fe13c3bd3fa24d1d576617548647614d38feb87
Pierre Etrillard <pierre.etrillard@live.fr>
2022-08-12 11:12:25 +0200

    correction du controller modify en traitement d'objet non stringifié par multer(const sauceObjet = null => const  sauceObjet = {}). Coté traitement json : déplacement de la fonction de suppression de l'ancienne image après l'actuatlisation du chemin d'accés de la nouvelle

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,8 +1,9 @@
 const Sauce = require("../models/sauces");
 const fs = require("fs");
-/* --- ASTUCE!: les middlewears suivant utilisent des spread (fr:décomposition) : '...'
+/* --- ASTUCE!: les middlewears suivant utilisent des spread operator(fr:décomposition) : '...'
 cette synthaxe permet d'appeler tous les itérables de tableau ou d'objets (toutes les 
 clés de chaque sauces requétées dans notre cas). Pour plus d'expliquations, voir :
+https://www.devenir-webmaster.com/V2/TUTO/CHAPITRE/JAVASCRIPT/56-destructuring-spread-operator/
 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Spread_syntax#exemple_interactif
 --- */
 
@@ -54,28 +55,30 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  let oldPics = null;
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      oldPics = sauce.imageUrl.split("/images/")[1];;
-      //supression du fichier image,
-      console.log(`images/${oldPics}`);
-      fs.unlink(`images/${oldPics}`,(err) => {
-        if (err) throw err;
-        console.log('Fichier supprimé !');
-     })}
-      );
-  let sauceObject = null;//Contenaire du corps de requéte
-  //Test si la requète contient un fichier (= stringifié par multer):
-  const isMulterReq = req.file ? {return:true} : { return:false };
+ 
+  let sauceObject = {};//Contenaire du corps de requéte
+  //Test si la requète contient un fichier form/data (= stringifié par multer):
+  const isMulterReq = req.file ? true :  false ;
 
-  if (isMulterReq ){ //si true: parser et traiter celle-ci à la maniére du middlewear précédent (createSauce).
-    // fs.unlink(`images/${JSON.parse(req.file.body.image)}`),
-
-    sauceObject =  {...JSON.parse(req.body.sauce),
+  if (isMulterReq ){ //si true (requète à parser et image à modifier):
+    //1 recupérer le chemin de l'ancienne image,
+    let oldPic = null
+    Sauce.findOne({ _id: req.params.id })
+    .then((oldSauce) => {
+      oldPic = oldSauce.imageUrl.split("/images/")[1];
+    //2 parser la requète et mettre à jour le chemin vers la nouvelle image,
+    sauceObject =  {...JSON.parse(req.body.sauce),  
     imageUrl: `${req.protocol}://${req.get("host")}/images/${ req.file.filename}`};
-      //supression de l'image précédante,
-  } else {   //Sinon récupération du corps de la requète 
+    //3 supression de l'ancienne image.
+      fs.unlink(`images/${oldPic}`,(err) => {
+        if (err) throw err;
+        console.log('Ancienne image supprimée !');
+     })}
+      )
+      .catch((error) => {
+        res.satus(400).json({ error });
+      });
+  } else {   //Si la requète ne contient pas de modification d'image ()
     sauceObject = {...req.body}}
 
   delete sauceObject._userId; //sécurité ! (cf middlewear précédent (createSauce))
@@ -99,38 +102,6 @@ exports.modifySauce = (req, res, next) => {
       res.satus(400).json({ error });
     });
 };
-// exports.modifySauce = (req, res, next) => {
-//   //Test si la requète contient un fichier (= stringifié par multer), parser et traiter celle-ci à la maniére du middlewear précédent (createSauce).
-//   const sauceObject = req.file
-//     ? {
-//         ...JSON.parse(req.body.sauce),
-//         imageUrl: `${req.protocol}://${req.get("host")}/images/${
-//           req.file.filename
-//         }`,
-//       } //Sinon récupération du corps de la requète
-//     : { ...req.body };
-
-//   delete sauceObject._userId; //sécurité ! (cf middlewear précédent (createSauce))
-//   //Ciblage de la sauce à modifier avec l'id présent dans l'url
-//   Sauce.findOne({ _id: req.params.id })
-//     .then((sauce) => {
-//       if (sauce.userId != req.auth.userId) {
-//         //si tentative de modification de la sauce d'un autre user:
-//         res.satus(401).json({ message: "Non-autorisé !" });
-//       } else {
-//         //Sinon :updateOne({objet à cibler dans la BDD}, {nouvelle version, correspondante à l'id déclaré ds le 1er paramètre})
-//         Sauce.updateOne(
-//           { _id: req.params.id },
-//           { ...sauceObject, _id: req.params.id }
-//         )
-//           .then(() => res.status(200).json({ message: "Sauce mise à jour !" }))
-//           .catch((error) => res.satus(401).json({ error }));
-//       }
-//     })
-//     .catch((error) => {
-//       res.satus(400).json({ error });
-//     });
-// };
 
 exports.deleteSauce = (req, res, next) => {
   //Ciblage de la sauce à modifier avec l'id présent dans l'url.
@@ -162,13 +133,10 @@ exports.deleteSauce = (req, res, next) => {
 exports.likeSauce = (req, res, next) => {
   Sauce.findById(req.params.id)
     .then((sauce) => {
-      console.log("sauce.usersLiked avant filtre : " + sauce.usersLiked);
       // Suppression de l'userId si déja présent dans les tableaux: usersLiked et usersDisliked
       let likersIds = sauce.usersLiked.filter(
         (idList) => idList !== req.auth.userId
       );
-      console.log("likersIds après filtre : " + likersIds);
-      console.log("sauce.usersLiked après filtre : " + sauce.usersLiked);
       let dislikersIds = sauce.usersDisliked.filter(
         (idList) => idList !== req.auth.userId
       );
@@ -176,38 +144,16 @@ exports.likeSauce = (req, res, next) => {
       switch (req.body.like) {
         case 1: // l'utilissateur like la sauce
           likersIds.push(req.auth.userId);
-          console.log("like = likersIds aprés push avant save : " + likersIds);
-          console.log(
-            "like = sauce.usersLiked aprés push avant save : " +
-              sauce.usersLiked
-          );
-
           break;
         case -1: // l'utilissateur dislike la sauce
           dislikersIds.push(req.auth.userId);
-          console.log(
-            "dislike = dislikersIds aprés push avant save : " + dislikersIds
-          );
-          console.log(
-            "dislike = sauce.usersDisliked aprés push avant save : " +
-              sauce.usersDisliked
-          );
           break;
       }
       sauce.usersLiked = likersIds;
       sauce.usersDisliked = dislikersIds;
       sauce.dislikes = dislikersIds.length;
       sauce.likes = likersIds.length;
-      console.log("likersIds aprés switch avant save : " + likersIds.length);
-      console.log(
-        "like = sauce.usersLiked aprés switch avant save : " +
-          sauce.usersLiked.length
-      );
-
       sauce.save().then(() => {
-        console.log(likersIds);
-        console.log(dislikersIds);
-
         res.status(200).json({ message: "appréciation enregistrée" });
       });
     })
