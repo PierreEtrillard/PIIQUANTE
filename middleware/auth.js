77b422e01d17ac02dcd38c0812bd810afa0b694e
Pierre Etrillard <pierre.etrillard@live.fr>
2022-08-18 11:55:23 +0200

    renommage du dossier middleware et mise à jour du readme

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+const tokenManager = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];//split extrait le token après le premiére espace (juste aprés le mot 'Bearer')
+    const decodedToken = tokenManager.verify(token, "RANDOM_TOKEN_SECRET");//"RANDOM_TOKEN_SECRET" à remplacer par une chaine aléatoire plus longue en phase de production
+    const userId = decodedToken.userId;
+    req.auth = { userId: userId };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
